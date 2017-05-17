b25e7d3cb7400f1b33e471084cfedd12bcc4a098
Linda "zig" Speight <linda@reachmaine.com>
2017-05-16 22:56:38 -0400

    remove share button via misc/share template

--- ea-classifieds/functions.php
+++ ea-classifieds/functions.php
@@ -7,21 +7,28 @@
  */
 
 // take off share buttons.
-//add_action( 'after_setup_theme', 'reach_theme_mods' );
+add_action( 'after_setup_theme', 'reach_theme_mods' );
 function reach_theme_mods() {
-   remove_action( 'superlist_after_listing_banner', 'superlist_render_share_button' );
-   remove_action( 'inventor_listing_actions', 'superlist_render_share_button');
+  
 }
 
 add_action('inventor_listing_content', 'reach_listing_archive_extra', 10, 2);
 function reach_listing_archive_extra( $listing_id, $display = null) {
-  $jobtype = get_post_meta( $listing_id, 'listing_parttime', true );
-  if ($jobtype) {
-      echo "<dt></dt><dd>".$jobtype."</dd>";
+   if ( $display == 'row' ) {
+    $jobtype = get_post_meta( $listing_id, 'listing_parttime', true );
+    if ($jobtype) {
+        echo "<dt></dt><dd>".$jobtype."</dd>";
+    }
+    $company= get_post_meta( $listing_id, 'listing_company', true );
+    if ($company) {
+        echo "<dt></dt><dd>".$company."</dd>";
+    }
   }
-  $company= get_post_meta( $listing_id, 'listing_company', true );
-  if ($company) {
-      echo "<dt></dt><dd>".$company."</dd>";
-  }
-
 }
+
+add_filter( 'inventor_filter_sort_by_choices', function( $choices ) {
+    if( array_key_exists( 'price', $choices ) ) {
+        unset( $choices['price'] );
+    }
+    return $choices;
+}, 11 );
