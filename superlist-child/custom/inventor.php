bdb69faa98919ae5f086997449ef5e10b6c2b628
Linda "zig" Speight <linda@reachmaine.com>
2017-06-02 13:48:13 -0400

    remove street view & inside view via filter

--- superlist-child/custom/inventor.php
+++ superlist-child/custom/inventor.php
@@ -64,3 +64,13 @@ function reach_listing_thumb( $int_listing_id) {
   }
 
 }
+
+// disable street view
+add_filter( 'inventor_metabox_field_enabled', 'disable_gmap_views', 10, 4 );
+function disable_gmap_views( $enabled, $metabox_id, $field_id, $post_type ) {
+    if ( ( 'listing_street_view' == $field_id ) || ('listing_inside_view' == $field_id) ) {
+        return false;
+    }
+
+    return $enabled;
+}
