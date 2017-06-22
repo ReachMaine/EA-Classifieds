78790a353bbd61cb5d376a5dd300dd714bb5bc80
Tim Suellentrop <tim@timsuellentrop.com>
2017-06-22 14:10:31 -0400

    Bootstrap CDN and Test Page

--- superlist-child/header.php
+++ superlist-child/header.php
@@ -18,6 +18,8 @@
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <script data-cfasync="false" type="text/javascript" src="//cdn.broadstreetads.com/init.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <?php if ( is_singular() ) { wp_enqueue_script( 'comment-reply' ); } ?>
 
     <?php wp_head(); ?>
