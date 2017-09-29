0df5af144e835b4d0aa7bb0ea794db4d6c219a0f
Linda "zig" Speight <linda@reachmaine.com>
2017-09-29 17:07:41 -0400

    dont show map location

--- superlist-child/custom/inventor.php
+++ superlist-child/custom/inventor.php
@@ -56,7 +56,8 @@ function reach_listing_thumb( $int_listing_id) {
 // disable street view
 add_filter( 'inventor_metabox_field_enabled', 'disable_gmap_views', 10, 4 );
 function disable_gmap_views( $enabled, $metabox_id, $field_id, $post_type ) {
-    if ( ( 'listing_street_view' == $field_id ) || ('listing_inside_view' == $field_id) ) {
+    $nope = array('listing_map_location',  'listing_street_view' , 'listing_inside_view', 'listing_map_location_polygon');
+    if ( in_array($field_id, $nope )  ) {
         return false;
     }
 
