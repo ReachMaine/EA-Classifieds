1aa6052bb48c6bab3a3992ae81e83d02a7a72892
Linda "zig" Speight <linda@reachmaine.com>
2017-10-02 10:37:13 -0400

    login redirect for everyone except admin

--- superlist-child/custom/branding.php
+++ superlist-child/custom/branding.php
@@ -82,3 +82,29 @@ function reach_product_wpseo_title($title) {
     }
     return $title;
 }
+
+/**
+ * Redirect user after successful login.
+ *
+ * @param string $redirect_to URL to redirect to.
+ * @param string $request URL the user is coming from.
+ * @param object $user Logged user's data.
+ * @return string
+ */
+
+function my_login_redirect( $redirect_to, $request, $user ) {
+	//is there a user to check?
+	if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+		//check for admins
+		if ( in_array( 'administrator', $user->roles ) ) {
+			// redirect them to the default place
+			return $redirect_to;
+		} else {
+			return home_url().'/my-listings/';
+		}
+	} else {
+		return $redirect_to;
+	}
+}
+
+add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
