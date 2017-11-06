b8cfe6291b3b51384ecdec8705d325e80fd12b70
Tim Suellentrop <tim@timsuellentrop.com>
2017-11-06 16:42:17 -0500

    put thumbnail in col3

--- superlist-child/templates/listings/detail/section-description.php
+++ superlist-child/templates/listings/detail/section-description.php
@@ -4,12 +4,19 @@
 global $post; ?>
 
 <?php // if ( ! empty( $post->post_content ) ) : ?>
-	<div class="listing-detail-section col-md-9" id="listing-detail-section-description">
+
+		<?php if ( has_post_thumbnail(get_the_ID()) ) {
+			echo '<div class="listing-detail-section col-md-3" id="listing-detail-section-image">';
+				reach_listing_thumb( get_the_ID());
+			echo '</div>';
+			echo '<div class="listing-detail-section col-md-6" id="listing-detail-section-description">';
+		}  else {
+			echo '<div class="listing-detail-section col-md-9" id="listing-detail-section-description">';
+		} ?>
+
 	    <?php /* <h2 class="page-header"><?php echo $section_title; ?></h2> */ ?>
 		<div class="listing-detail-description">
-			<?php if ( has_post_thumbnail(get_the_ID()) ) {
-				reach_listing_thumb( get_the_ID());
-			} ?>
+
 	    	<?php the_content(); ?>
 			<?php $targetpage = get_post_meta(get_the_ID(), INVENTOR_LISTING_PREFIX .'realtorpage', true);
 			if ($targetpage) {
